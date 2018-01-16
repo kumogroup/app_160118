2a2070824c45e9dec5baf0894c600de49ec61cf8
kumogroup <35257109+kumogroup@users.noreply.github.com>
2018-01-16 12:56:05 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+	$("body").on("click",".click_evt", function(){
+		var _action = $(this).attr("action");
+		var _id = $(this).attr("id");
+		switch(_action){
+			case "product":
+				
+				break;
+			case "store":
+				
+				break;
+			default:
+				break;
+		}
+	});
+});
